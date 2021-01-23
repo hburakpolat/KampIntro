8923aeb57082c156410cc8ce0f60141921498501
burak.polat <burak.polat@BURAKPOLAT>
2021-01-24 00:05:02 +0300

    5.ders

---
+++ OOP2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Burak";
+            musteri1.Soyadi = "Polat";
+            musteri1.TcNo = "1234567890";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "0987654321";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+
+        }
+    }
+}
